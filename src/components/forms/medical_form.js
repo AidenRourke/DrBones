003fdc3c0ffc3273376b3ad05714039b76abf3b6
Bryanrattanavong <arandomexample@hotmail.com>
2018-11-03 13:30:59 -0400

    Form created

---
+++ src/components/forms/medical_form.js
@@ -0,0 +1,46 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+
+class MedicalForm extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      medicalCondition: "",
+      date: "",
+      optionalNotes: "",
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  };
+
+  handleChange(event){
+    this.setState({[event.target.name]: event.target.value});
+  }
+  handleSubmit(event){
+
+  }
+  render(){
+    return(
+      <form onSubmit={this.handleChange}>
+        <label>
+          Medical Condition:
+          <input type="text" name="medicalCond" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <label>
+          Date:
+          <input type="text" name="date" value={this.state.value} onChange={this.handleChange}/>
+        </label>
+        <label>
+          Note(Optional):
+          <input type="textField" name="note" value={this.state.value} onChange={this.handleChange} placeHolder="Optional"/>
+        </label>
+        <input type="submit" value="Submit"/>
+      </form>
+    );
+  }
+}
+
+export default MedicalForm;
