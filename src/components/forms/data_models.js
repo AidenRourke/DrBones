545ecc0fa374f3801511bb584d3bf0480426c491
Bryan Rattanavong <arandomexample@hotmail.com>
2018-11-04 00:43:39 -0400

    placeholders n description in data models (#6)

--- src/components/forms/data_models.js
+++ src/components/forms/data_models.js
@@ -1,23 +1,23 @@
 export const dataModels = {
     medicalCondition: [
-        {name: "name", type: "string", display: "Title", required: true},
+        {name: "name", type: "string", display: "Title",description:"Enter the name of the medical condition.", required: true},
         {name: "date", type: "date", display: "Date", required: true},
-        {name: "notes", type: "array", display: "Notes"}
+        {name: "notes", type: "array", display: "Notes",description:"Optional."}
     ],
     operations: [
-        {name: "medicalConditionId", type: "string", display: "Medical Condition", required: true},
-        {name: "name", type: "string", display: "Title", required: true},
+        {name: "medicalConditionId", type: "string", display: "Medical Condition",description:"Enter the name of the medical condition.", required: true},
+        {name: "name", type: "string", display: "Title",description:"Enter the name of the operation.", required: true},
         {name: "date", type: "date", display: "Date", required: true},
-        {name: "notes", type: "array", display: "Notes"}
+        {name: "notes", type: "array", display: "Notes",description:"Optional."}
     ],
     medication: [
-        {name: "medicalConditionId", type: "string", display: "Medical Condition",Description:"Enter the name of the medical condition", required: true},
-        {name: "name", type: "string", display: "Title",Description:"Enter the name of the operation", required: true},
+        {name: "medicalConditionId", type: "string", display: "Medical Condition",description:"Enter the name of the medical condition.", required: true},
+        {name: "name", type: "string", display: "Title",description:"Enter the name of the medication.", required: true},
         {name: "date", type: "date", display: "Date", required: true},
-        {name: "dose", type: "string", display: "Dose", required: true},
-        {name: "frequency", type: "string", display: "Frequency", required: true},
-        {name: "timePeriod", type: "string", display: "Time Period", required: true},
-        {name: "notes", type: "array", display: "Notes"},
+        {name: "dose", type: "string", display: "Dose",description:"Enter the dosage given", required: true},
+        {name: "frequency", type: "string", display: "Frequency",description:"Enter how often the medication was taken (ex. once a day).", required: true},
+        {name: "timePeriod", type: "string", display: "Time Period",description:"Enter the time period the medication was taken (ex. 2 weeks).", required: true},
+        {name: "notes", type: "array", display: "Notes",description:"Optional."},
     ]
 
 };
