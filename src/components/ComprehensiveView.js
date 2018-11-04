9f1ba123148df0e03abc502ede5d54b20cef883b
Bryanrattanavong <arandomexample@hotmail.com>
2018-11-04 11:23:31 -0500

    Update

--- src/components/ComprehensiveView.js
+++ src/components/ComprehensiveView.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import styled from "styled-components";
 import axios from 'axios';
 import EditIcon from "./EditIcon";
-import {formatDate} from "../../src/utils/utils";
+import { formatDate } from "../../src/utils/utils";
 import Button from "./Button";
 
 const ViewContainer = styled.div`
@@ -40,69 +40,68 @@ const ViewContainer = styled.div`
 
 export default class ComprehensiveView extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            name: null,
-            uniqueId: this.props.uniqueId,
-            date: null
-        }
-    }
+	constructor( props ){
+		super( props );
+		this.state = {
+			name: null,
+			uniqueId: this.props.uniqueId,
+			date: null
+		}
+	}
 
-    componentWillMount(){
-        this.apiCall((data) => {
-            if (this.props.context.widgetType === 'medication') {
-                this.setState({
-                    medicalConditionId: data.medicalConditionId,
-                    name: data.name,
-                    date: data.date,
-                    dose: data.dose,
-                    frequency: data.frequency,
-                    timePeriod: data.timePeriod,
-                    notes: data.notes
-                });
-                this.getAllMedicalConditions();
-            }
-            if (this.props.context.widgetType === 'operation') {
+	componentWillMount(){
+		this.apiCall( ( data ) => {
+			if( this.props.context.widgetType === 'medication' ){
+				this.setState( {
+					medicalConditionId: data.medicalConditionId,
+					name: data.name,
+					date: data.date,
+					dose: data.dose,
+					frequency: data.frequency,
+					timePeriod: data.timePeriod,
+					notes: data.notes
+				} );
+				this.getAllMedicalConditions();
+			}
+			if( this.props.context.widgetType === 'operation' ){
 
-                this.setState({
-                    medicalConditionId: data.medicalConditionId,
-                    name: data.name,
-                    date: data.date,
-                    notes: data.notes
-                });
-                this.getAllMedicalConditions();
-            }
-            if (this.props.context.widgetType === 'medicalCondition') {
-                this.setState({
-                    name: data.name,
-                    date: data.date,
-                    notes: data.notes
-                });
-                this.getPrescriptions();
-                this.getOperations();
-            }
-        });
+				this.setState( {
+					medicalConditionId: data.medicalConditionId,
+					name: data.name,
+					date: data.date,
+					notes: data.notes
+				} );
+				this.getAllMedicalConditions();
+			}
+			if( this.props.context.widgetType === 'medicalCondition' ){
+				this.setState( {
+					medicalConditionId: data.uniqueId,
+					name: data.name,
+					date: data.date,
+					notes: data.notes
+				} );
+				this.getPrescriptions();
+				this.getOperations();
+			}
+		} );
 
-    }
+	}
 
-    async getPrescriptions() {
-        let url = 'http://localhost:4000/getAllMedications';
+	async getPrescriptions(){
+		let url = 'http://localhost:4000/getAllMedications';
 
         const response = await axios.post(url, {
-            userId: document.cookie,
-            uniqueId: this.props.context.uniqueId
+            userId: document.cookie
         });
 
-        let index;
-        let arr;
-        let arrOfPrescriptions = [];
+		let index;
+		let arr;
+		let arrOfPrescriptions = [];
 
         if (!response.data.error) {
             arr = response.data.results;
             for (index in arr) {
-                console.log(arr);
-                if(arr[index].unique_Id === this.props.medicalConditionId){
+                if(arr[index].medicalConditionId === this.state.medicalConditionId){
                     arrOfPrescriptions.push(arr[index].name);
                 }
             };
@@ -111,164 +110,166 @@ export default class ComprehensiveView extends Component {
             this.setState({failed: true});
         }
 
-    }
+	}
 
-    async getOperations() {
-        let url = 'http://localhost:4000/getAllOperations';
+	async getOperations(){
+		let url = 'http://localhost:4000/getAllOperations';
 
-        const response = await axios.post(url, {
-            userId: document.cookie,
-            uniqueId: this.props.context.uniqueId
-        });
+		const response = await axios.post( url, {
+			userId: document.cookie,
+			uniqueId: this.props.context.uniqueId
+		} );
 
-        let index;
-        let arr;
-        let arrOfOperations = [];
+		let index;
+		let arr;
+		let arrOfOperations = [];
 
-        if (!response.data.error) {
-            arr = response.data.results;
-            for (index in arr) {
-                console.log(arr);
-                if(arr[index].unique_Id === this.props.medicalConditionId){
-                    arrOfOperations.push(arr[index].name);
-                }
-            };
-            console.log(arrOfOperations);
-            this.setState({operations: arrOfOperations});
-        } else {
-            this.setState({failed: true});
-        }
+		if( !response.data.error ){
+			arr = response.data.results;
+			console.log(this.state.medicalConditionId)
+			console.log(arr)
+			for( index in arr ){
+				if( arr[index].medicalConditionId === this.state.medicalConditionId ){
+					arrOfOperations.push( arr[index].name );
+				}
+			}
+			;
+			this.setState( { operations: arrOfOperations } );
+		} else {
+			this.setState( { failed: true } );
+		}
 
-    }
+	}
 
-    async apiCall(callback) {
-        let url;
-        if (this.props.context.widgetType === 'medication') {
-            url = 'http://localhost:4000/getMedicationInfo';
-        }
-        if (this.props.context.widgetType === 'operation') {
-            url = 'http://localhost:4000/getOperationInfo';
-        }
-        if (this.props.context.widgetType === 'medicalCondition') {
-            url = 'http://localhost:4000/getMedicalConditionInfo';
-        }
+	async apiCall( callback ){
+		let url;
+		if( this.props.context.widgetType === 'medication' ){
+			url = 'http://localhost:4000/getMedicationInfo';
+		}
+		if( this.props.context.widgetType === 'operation' ){
+			url = 'http://localhost:4000/getOperationInfo';
+		}
+		if( this.props.context.widgetType === 'medicalCondition' ){
+			url = 'http://localhost:4000/getMedicalConditionInfo';
+		}
 
-        const response = await axios.post(url, {
-            userId: document.cookie,
-            uniqueId: this.props.context.uniqueId
-        });
+		const response = await axios.post( url, {
+			userId: document.cookie,
+			uniqueId: this.props.context.uniqueId
+		} );
 
-        if (!response.data.error) {
-            callback(response.data.results[0]);
-        } else {
-            this.setState({failed: true});
-        }
-    }
+		if( !response.data.error ){
+			callback( response.data.results[0] );
+		} else {
+			this.setState( { failed: true } );
+		}
+	}
 
-    async getAllMedicalConditions(){
-        const response = await axios.post('http://localhost:4000/getAllMedicalConditions', {
-            userId: document.cookie
-        });
-        let arr = response.data.results;
-        let condition;
-        let medicalConditionString;
-        for (condition in arr) {
-            if(arr[condition].uniqueId === this.state.medicalConditionId){
-                medicalConditionString = arr[condition].name;
-            }
-        };
-        this.setState({condition: response.data.results, medicalConditionString: medicalConditionString});
-    }
+	async getAllMedicalConditions(){
+		const response = await axios.post( 'http://localhost:4000/getAllMedicalConditions', {
+			userId: document.cookie
+		} );
+		let arr = response.data.results;
+		let condition;
+		let medicalConditionString;
+		for( condition in arr ){
+			if( arr[condition].uniqueId === this.state.medicalConditionId ){
+				medicalConditionString = arr[condition].name;
+			}
+		}
+		;
+		this.setState( { condition: response.data.results, medicalConditionString: medicalConditionString } );
+	}
 
-    render() {
-        const {onEdit, onClose} = this.props;
-        let jsx;
-        if (this.props.context.widgetType === 'medication') {
-            jsx = (
-                <ViewContainer>
-                    <div className='container animated fadeInUp'>
-                    <EditIcon onClick={() => onEdit(this.state)}/>
-                    <h1>{`${this.state.name}`}</h1>
-                    <div className={'line'}>
-                        <h1>{`To Treat: `}</h1>
-                        <h1>{`${this.state.medicalConditionString}`}</h1>
-                    </div>
-                    <div className={'line'}>
-                        <h1>{`Date:`}</h1>
-                        <h1>{`${formatDate(this.state.date)}`}</h1>
-                    </div>
-                    <div className={'line'}>
-                        <h1>{`Dose:`}</h1>
-                        <h1>{`${this.state.dose}`}</h1>
-                    </div>
-                    <div className={'line'}>
-                        <h1>{`Frequency:`}</h1>
-                        <h1>{`${this.state.frequency}`}</h1>
-                    </div>
-                    <div className={'line'}>
-                        <h1>{`Duration:`}</h1>
-                        <h1>{`${this.state.timePeriod}`}</h1>
-                    </div>
-                    <div className={'line'}>
-                        <h1>{`Notes:`}</h1>
-                        <h1>{`${this.state.notes ? this.state.notes : 'No notes recorded!'} `}</h1>
-                    </div>
-					<Button className='button' muted onClick={onClose}>Back</Button>
-                    </div>
-                </ViewContainer>
-            );
-        } else if (this.props.context.widgetType === 'operation') {
-            jsx = (
-                <ViewContainer>
-                    <div className='container'>
-                    <EditIcon onClick={() => onEdit(this.state)}/>
-                    <h1>{`${this.state.name}`}</h1>
-                    <div className={'line'}>
-                        <h1>{`To Treat: `}</h1>
-                        <h1>{`${this.state.medicalConditionString}`}</h1>
-                    </div>
-                    <div className={'line'}>
-                        <h1>{`Date:`}</h1>
-                        <h1>{`${formatDate(this.state.date)}`}</h1>
-                    </div>
-                    <div className={'line'}>
-                        <h1>{`Notes:`}</h1>
-                        <h1>{`${this.state.notes ? this.state.notes : 'No notes recorded!'} `}</h1>
-                    </div>
-					<Button className='button' muted onClick={onClose}>Back</Button>
-                    </div>
-                </ViewContainer>
-            );
-        } else if (this.props.context.widgetType === 'medicalCondition') {
-            jsx = (
-                <ViewContainer>
-                    <div className='container'>
-                    <EditIcon onClick={() => onEdit(this.state)}/>
-                    <h1>{`${this.state.name}`}</h1>
-                    <div className={'line'}>
-                        <h1>{`Date:`}</h1>
-                        <h1>{`${formatDate(this.state.date)}`}</h1>
-                    </div>
-                    <div className={'line'}>
-                        <h1>{`Notes:`}</h1>
-                        <h1>{`${this.state.notes ? this.state.notes : 'No notes recorded!'} `}</h1>
-                    </div>
-                    <div className={'line'}>
-                        <h1>{`Prescribed Medications:`}</h1>
-                        <h1>{`${this.state.prescriptions && this.state.prescriptions.length !== 0 ? this.state.prescriptions: 'No prescribed medications!'} `}</h1>
-                    </div>
-                    <div className={'line'}>
-                        <h1>{`Operations Done:`}</h1>
-                        <h1>{`${this.state.operations && this.state.operations.length !== 0 ? this.state.operations: 'No operations done!'} `}</h1>
-                    </div>
-                    <Button className='button' muted onClick={onClose}>Back</Button>
-                    </div>
-                </ViewContainer>
-            );
-        } else {
-            jsx = '';
-        }
-        return jsx;
-    }
+	render(){
+		const { onEdit, onClose } = this.props;
+		let jsx;
+		if( this.props.context.widgetType === 'medication' ){
+			jsx = (
+				<ViewContainer>
+					<div className='container animated fadeInUp'>
+						<EditIcon onClick={() => onEdit( this.state )}/>
+						<h1>{`${this.state.name}`}</h1>
+						<div className={'line'}>
+							<h1>{`To Treat: `}</h1>
+							<h1>{`${this.state.medicalConditionString}`}</h1>
+						</div>
+						<div className={'line'}>
+							<h1>{`Date:`}</h1>
+							<h1>{`${formatDate( this.state.date )}`}</h1>
+						</div>
+						<div className={'line'}>
+							<h1>{`Dose:`}</h1>
+							<h1>{`${this.state.dose}`}</h1>
+						</div>
+						<div className={'line'}>
+							<h1>{`Frequency:`}</h1>
+							<h1>{`${this.state.frequency}`}</h1>
+						</div>
+						<div className={'line'}>
+							<h1>{`Duration:`}</h1>
+							<h1>{`${this.state.timePeriod}`}</h1>
+						</div>
+						<div className={'line'}>
+							<h1>{`Notes:`}</h1>
+							<h1>{`${this.state.notes ? this.state.notes : 'No notes recorded!'} `}</h1>
+						</div>
+						<Button className='button' muted onClick={onClose}>Back</Button>
+					</div>
+				</ViewContainer>
+			);
+		} else if( this.props.context.widgetType === 'operation' ){
+			jsx = (
+				<ViewContainer>
+					<div className='container'>
+						<EditIcon onClick={() => onEdit( this.state )}/>
+						<h1>{`${this.state.name}`}</h1>
+						<div className={'line'}>
+							<h1>{`To Treat: `}</h1>
+							<h1>{`${this.state.medicalConditionString}`}</h1>
+						</div>
+						<div className={'line'}>
+							<h1>{`Date:`}</h1>
+							<h1>{`${formatDate( this.state.date )}`}</h1>
+						</div>
+						<div className={'line'}>
+							<h1>{`Notes:`}</h1>
+							<h1>{`${this.state.notes ? this.state.notes : 'No notes recorded!'} `}</h1>
+						</div>
+						<Button className='button' muted onClick={onClose}>Back</Button>
+					</div>
+				</ViewContainer>
+			);
+		} else if( this.props.context.widgetType === 'medicalCondition' ){
+			jsx = (
+				<ViewContainer>
+					<div className='container'>
+						<EditIcon onClick={() => onEdit( this.state )}/>
+						<h1>{`${this.state.name}`}</h1>
+						<div className={'line'}>
+							<h1>{`Date:`}</h1>
+							<h1>{`${formatDate( this.state.date )}`}</h1>
+						</div>
+						<div className={'line'}>
+							<h1>{`Notes:`}</h1>
+							<h1>{`${this.state.notes ? this.state.notes : 'No notes recorded!'} `}</h1>
+						</div>
+						<div className={'line'}>
+							<h1>{`Prescribed Medications:`}</h1>
+							<h1>{`${this.state.prescriptions && this.state.prescriptions.length !== 0 ? this.state.prescriptions : 'No prescribed medications!'} `}</h1>
+						</div>
+						<div className={'line'}>
+							<h1>{`Operations Done:`}</h1>
+							<h1>{`${this.state.operations && this.state.operations.length !== 0 ? this.state.operations : 'No operations done!'} `}</h1>
+						</div>
+						<Button className='button' muted onClick={onClose}>Back</Button>
+					</div>
+				</ViewContainer>
+			);
+		} else {
+			jsx = '';
+		}
+		return jsx;
+	}
 }
 
